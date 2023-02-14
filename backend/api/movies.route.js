6e15f31671387debe5511f03f8f29bacc2549ae9
Flcable <flcable@gmail.com>
2023-02-14 19:20:00 +0000

    Reviews Controller

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import MoviesController from '../dao/movies.Controller.js';
+import ReviewsController from './reviews.Controller.js';
 
 const router = express.Router();
 
 router.route('/').get(MoviesController.apiGetMovies);
+router
+  .route('/review')
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview);
 
 export default router;
